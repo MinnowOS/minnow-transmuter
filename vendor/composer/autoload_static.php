0f1a55800059933495d81f9a1d91a1af20913599
Austin Ginder <austinginder@gmail.com>
2025-08-02 08:17:09 -0400

    👌 IMPROVE: Composer update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,6 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInit9622dd827becd4430ae91af9e0a4face
 {
     public static $files = array (
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
     );
 
